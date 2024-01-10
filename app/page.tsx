ea7ddbad097b4d5c46ebb456cc94040e169a0d79
Joel <joelbaraka4325@gmail.com>
2024-01-10 21:29:50 +0300

    header image component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h2>Me</h2>
+    <main className="">
+      <h2>Booking.com</h2>
     </main>
   );
 }
