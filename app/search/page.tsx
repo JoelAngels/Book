7935c27b76adc148936e98812d1e875095e571bd
Joel <joelbaraka4325@gmail.com>
2024-01-23 16:53:04 +0300

    building the search page

---
+++ app/search/page.tsx
@@ -0,0 +1,28 @@
+import { notFound } from "next/navigation";
+import React from "react";
+
+type Props = {
+  searchParams: SearchParams;
+};
+
+type SearchParams = {
+  url: URL;
+  group_adults: string;
+  group_children: string;
+  no_rooms: string;
+  checkin: string;
+  checkout: string;
+};
+
+// Distructure props to get the searchParams
+async function SearchPage({ searchParams }: Props) {
+  // if their is no search url, return not found
+  if (!searchParams.url) return notFound;
+
+  const results = await fetchResults(searchParams);
+
+  if (!results) return <div>Search Page</div>;
+  return <div>Search Page</div>;
+}
+
+export default SearchPage;
