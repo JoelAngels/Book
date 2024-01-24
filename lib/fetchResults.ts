10e9c45c68ee444f949a3bd5272afda2f07e775e
Joel <joelbaraka4325@gmail.com>
2024-01-24 14:17:02 +0300

    getting the selectors

--- lib/fetchResults.ts
+++ lib/fetchResults.ts
@@ -12,12 +12,121 @@ export async function fetchResults(searchParams: SearchParams) {
     // Loop through to reconstruct the url
     Object.keys(searchParams).forEach((key) => {
         if (key === "url" || key === "location") return;
+
+        // Check the value and check if its the key of type SearchParams
     
         const value = searchParams[key as keyof SearchParams];
     
+        // then append it 
+
         if (typeof value === "string") {
           url.searchParams.append(key, value);
         }
       });
 console.log("scraping url", url.href)
+
+  const body = {
+    source: "universal",
+    url: url.href,
+    parse: true,
+    render: "html",
+      parsing_instructions: {
+        listings: {
+          _fns: [
+            {
+              _fn: "xpath",
+              _args: ["//div[@data-testid='property-card-container']"],
+            },
+          ],
+          _items: {
+            title: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  _args: [".//div[@data-testid='title']/text()"],
+                },
+              ],
+            },
+            description: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  _args: [
+                    ".//h4[contains(@class, 'abf093bdfe e8f7c070a7')]/text()",
+                  ],
+                },
+              ],
+            },
+            booking_metadata: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  // Go to the div that contains this class c..., once you find it, 
+                  // enter the other div that contains the class then get me the text 
+                  _args: [
+                    ".//div[contains(@class, 'c5ca594cb1 f19ed67e4b')]/div[contains(@class, 'abf093bdfe f45d8e4c32')]/text()",
+                  ],
+                },
+              ],
+            },
+            link: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  // Find the a tag that contains this class, a... then give me the link href
+                  _args: [".//a[contains(@class, 'a78ca197d0')]/@href"],
+                },
+              ],
+            },
+            price: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  _args: [
+                    `.//span[contains(@class, 'f6431b446c fbfd7c1165 e84eb96b1f')]/text()`,
+                  ],
+                },
+              ],
+            },
+            url: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  _args: [".//img/@src"],
+                },
+              ],
+            },
+            rating_word: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  _args: [
+                    ".//div[@class='a3b8729ab1 e6208ee469 cb2cbb3ccb']/text()",
+                  ],
+                },
+              ],
+            },
+            rating: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  _args: [".//div[@class='a3b8729ab1 d86cee9b25']/text()"],
+                },
+              ],
+            },
+            rating_count: {
+              _fns: [
+                {
+                  _fn: "xpath_one",
+                  _args: [
+                    ".//div[@class='abf093bdfe f45d8e4c32 d935416c47']/text()",
+                  ],
+                },
+              ],
+            },
+          },
+        },
+    }
 }
+}
+
